bdfb8b0af0bccb24a9cd866dd1cc654c2f718d2b
Nilufa1 <nilufasultana47@gmail.com>
2025-06-13 02:00:47 +0800

    About and Terms&Policy page  change

--- ui/src/About.js
+++ ui/src/About.js
@@ -7,16 +7,27 @@ const About = () => {
   const navigate = useNavigate();
   return (
     <div className="about-container">
-      <h1>About This Journal App</h1>
+      <h2>About This Journal App</h2>
       <p>
-        Welcome to your personal Journal App – a secure and intuitive platform designed to help you record your thoughts, track your goals, and reflect on your daily experiences.
+        Welcome to your personal Journal – our vision was to create a secure and intuitive platform designed to help users record their thoughts and reflect on daily experiences to improve themselves.
       </p>
       <p>
-        This app offers features like custom journal entries, dark mode, personalized themes, habit tracking, and much more, all tailored for self-improvement and mental clarity.
+        It offers features like custom journal entries, dark mode, analysing your journal and gives you results to reflect on that, all tailored for self-improvement and mental clarity.
       </p>
       <p>
-        Your data is private and remains stored only on your device, unless you choose to back it up externally.
+        Users data is private and remains stored only on their device, cause we have no idea what to do with it further.
       </p>
+      <p>
+        It is a personal and secure digital space designed to help users reflect,
+        record, and revisit their thoughts and experiences. Whether you're logging daily events,
+        tracking emotions, or preserving memories,  makes journaling intuitive, accessible,
+        and meaningful.
+      </p>
+      <p> Obviously if users know how to write all their thoughts.</p>
+      <p>We belive we should acknowledge everyones hard work and efforts and this Journal App is the result of the dedication and creativity of contributors:</p>
+      <p> Swastik Acharya</p>
+      <p>Suhana Zaman</p>
+      <p> Nilufa Sultana</p>
       <button onClick={() => navigate(-1)}>⬅ Back</button>
     </div>
   );
--- ui/src/Policy.js
+++ ui/src/Policy.js
@@ -7,19 +7,17 @@ const Policy = () => {
   return (
     <div className="policy-container">
       <h1>Terms & Policies</h1>
-      <p>
+      <h4>
         Welcome to our Terms and Policies page. By using this app, you agree to
         the following terms:
-      </p>
-      <ul>
-        <li>Your journal data is private and stored securely.</li>
-        <li>You are responsible for safeguarding your login credentials.</li>
-        <li>We do not sell your personal data to third parties.</li>
-        <li>By continuing, you agree to our use of cookies and analytics.</li>
-        <li>
-          Any inappropriate use of the app may result in account suspension.
-        </li>
-      </ul>
+      </h4>
+      <p>Your journal data is private and stored securely.
+         You are responsible for safeguarding your login credentials.
+         We do not sell your personal data to third parties.
+         By continuing, you agree to our use of cookies and analytics.
+         Any inappropriate use of the app may result in account suspension.
+        </p>
+
       <p>These terms may be updated periodically. Please check back often.</p>
       <Link to="/Homepage" className="back-link">← Back to Homepage</Link>
     </div>
